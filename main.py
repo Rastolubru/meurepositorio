1bfaf7f01edcf33e7f1a65da5ba66072e47ed2a8
Ralubrusto <ralubrusto.27@gmail.com>
2016-05-19 10:20:12 -0300

    Não esqueça de dar o nome pras vaquinhas

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import pedágio.py
+
+	#fique brabo por ter que pagar pedagio
 main:
 	print "Esta é a melhor fazenda do sul do mundo"
 
 Curral:
 	#Abrir a porteira
 	#Deixar a boiada passar
+	#Dê um nome pra cada ruminante que passar
 	#Tocar o berrante
